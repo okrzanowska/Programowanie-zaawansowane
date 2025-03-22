5cafd0952358e0ac300648fcb7598a74c5b27495
Oliwia <oliwia.krzanowska.ok@gmail.com>
2025-03-22 21:31:38 +0100

    Programowanie zaawansowane - zajęcia 22.03.2025

---
+++ Ciasto/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// Klasa Ciasto - reprezentuje ciasto, czyli nazwę, rodzaj i składniki
+public class Ciasto
+{
+    public string Nazwa { get; set; } // Nazwa ciasta
+    public string Rodzaj { get; set; } // Rodzaj ciasta
+    public List<string> Skladniki { get; set; } // Lista składników ciasta
+
+    // Konstruktor klasy Ciasto - inicjalizuje właściwości nazwa, rodzaj i składniki
+    public Ciasto(string nazwa, string rodzaj, List<string> skladniki)
+    {
+        Nazwa = nazwa; // Ustawia nazwę ciasta
+        Rodzaj = rodzaj; // Ustawia rodzaj ciasta
+        Skladniki = skladniki; // Ustawia składniki ciasta
+    }
+}
+
+// Interfejs IFabrykaCiasta - definiuje metodę, która tworzy ciasto
+public interface IFabrykaCiasta
+{
+    Ciasto StwórzCiasto(); // Metoda do tworzenia ciasta
+}
+
+// Klasa FabrykaCiastaCzekoladowego - implementuje interfejs IFabrykaCiasta
+public class FabrykaCiastaCzekoladowego : IFabrykaCiasta
+{
+    // Metoda do tworzenia ciasta czekoladowego
+    public Ciasto StwórzCiasto()
+    {
+        var skladniki = new List<string> { "Czekolada", "Mąka", "Jajka", "Masło" }; // Składniki ciasta czekoladowego
+        return new Ciasto("Czekoladowe", "Kruche", skladniki); // Zwraca nowe ciasto czekoladowe
+    }
+}
+
+// Klasa FabrykaCiastaJabłkowego - implementuje interfejs IFabrykaCiasta
+public class FabrykaCiastaJabłkowego : IFabrykaCiasta
+{
+    // Metoda do tworzenia ciasta jabłkowego
+    public Ciasto StwórzCiasto()
+    {
+        var skladniki = new List<string> { "Jabłka", "Cynamon", "Mąka", "Cukier" }; // Składniki ciasta jabłkowego
+        return new Ciasto("Jabłkowe", "Drożdżowe", skladniki); // Zwraca nowe ciasto jabłkowe
+    }
+}
+
+// Klasa PlanPieczenia - reprezentuje plan pieczenia ciast
+public class PlanPieczenia : IEnumerable<Ciasto>
+{
+    private List<Ciasto> ciasta = new List<Ciasto>(); // Lista ciast w planie pieczenia
+
+    // Metoda do dodawania ciasta do planu pieczenia
+    public void DodajCiasto(IFabrykaCiasta fabryka)
+    {
+        Ciasto ciasto = fabryka.StwórzCiasto(); // Tworzy ciasto za pomocą fabryki
+        ciasta.Add(ciasto); //Dodaje ciasto do listy ciast
+    }
+
+    // Metoda do wyświetlania planu pieczenia
+    public void WyświetlPlan()
+    {
+        foreach (var ciasto in ciasta) // Iteruje przez wszystkie ciasta w planie
+        {
+            // Wyświetla nazwę, rodzaj i składniki ciasta
+            Console.WriteLine($"Nazwa: {ciasto.Nazwa}, Rodzaj: {ciasto.Rodzaj}, Składniki: {string.Join(", ", ciasto.Skladniki)}");
+        }
+    }
+
+    // Implementacja interfejsu IEnumerable<Ciasto>
+    public IEnumerator<Ciasto> GetEnumerator()
+    {
+        return ciasta.GetEnumerator(); // Zwraca enumerator dla listy ciast
+    }
+
+    // Implementacja interfejsu IEnumerable
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator(); // Zwraca enumerator dla listy ciast
+    }
+}
+
+// Klasa główna programu
+class Program
+{   
+    // Metoda główna programu
+    static void Main(string[] args)
+    {
+        var planPieczenia = new PlanPieczenia(); // Tworzy nowy plan pieczenia
+
+        var fabrykaCzekoladowego = new FabrykaCiastaCzekoladowego(); // Tworzy fabrykę ciasta czekoladowego
+        var fabrykaJabłkowego = new FabrykaCiastaJabłkowego(); // Tworzy fabrykę ciasta jabłkowego
+
+        planPieczenia.DodajCiasto(fabrykaCzekoladowego); // Dodaje ciasto czekoladowe do planu pieczenia
+        planPieczenia.DodajCiasto(fabrykaJabłkowego); // Dodaje ciasto jabłkowe do planu pieczenia
+ 
+        Console.WriteLine("Plan pieczenia:"); // Wyświetla nagłówek dla planu pieczenia
+        planPieczenia.WyświetlPlan(); // Wywołuje metodę do wyświetlania planu pieczenia
+    }
+}
---
+++ NPC/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace NPC {
+
+    //Interfejs NPC - posiada metodę PrzedstawSię
+    public interface INPC {
+        void PrzedstawSię(); //Metoda PrzedstawSię
+    }
+
+    // Klasa Wojownik - implementuje interfejs INPC
+    public class Wojownik : INPC {
+        //Implementacja metody PrzedstawSię dla klasy Wojownik
+        public void PrzedstawSię() {
+            Console.WriteLine("Jestem Wojownikiem, walczę mieczem"); // Wyświetla przedstawienie Wojownika
+        }
+    }
+
+    // Klasa Mag - implementuje interfejs INPC
+    public class Mag : INPC {
+        // Implementacja metody PrzedstawSię dla klasy Mag
+        public void PrzedstawSię() {
+            Console.WriteLine("Jestem Magiem, władającym magią żywiołów"); // Wyświetla przedstawienie Maga
+        }
+    }
+    // Klasa Złodziej - implementuje interfejs INPC
+    public class Złodziej : INPC {
+        // Implementacja metody PrzedstawSię dla klasy Złodziej
+        public void PrzedstawSię() {
+            Console.WriteLine("Jestem Złodziejem, nie mam atrybutów"); // Wyświetla przedstawienie Złodzieja
+        }
+    }
+
+    // Interfejs IFabrykaNPC - definiuje metodę fabryki, która tworzy NPC
+    public interface IFabrykaNPC {
+        INPC StwórzNPC(); // Metoda do tworzenia NPC
+    }
+
+    // Klasa FabrykaWojownika - implementuje interfejs IFabrykaNPC
+    public class FabrykaWojownika : IFabrykaNPC {
+        // Implementacja metody StwórzNPC, która zwraca nowego NPC - Wojownika
+        public INPC StwórzNPC() {
+            return new Wojownik(); // Zwraca nowego Wojownika
+        }
+    }
+
+    // Klasa FabrykaMaga - implementuje interfejs IFabrykaNPC
+    public class FabrykaMaga : IFabrykaNPC {
+        // Implementacja metody StwórzNPC, która zwraca nowego NPC - Maga
+        public INPC StwórzNPC() {
+            return new Mag(); // Zwraca nowego Maga
+        }
+    }
+
+    // Klasa FabrykaZłodzieja - implementuje interfejs IFabrykaNPC
+    public class FabrykaZłodzieja : IFabrykaNPC {
+        // Implementacja metody StwórzNPC, która zwraca nowego NPC - Złodzieja
+        public INPC StwórzNPC() {
+            return new Złodziej(); // Zwraca nowego Złodzieja
+        }
+    }
+
+    // Klasa główna programu
+    public class Program {
+        // Metoda główna programu
+        public static void Main() {
+            Random random = new Random(); // Inicjalizacja generatora liczb losowych
+            int typPostaci = random.Next(0, 3); // Losowanie typu postaci (0 - Wojownik, 1 - Mag, 2 - Złodziej)
+
+            IFabrykaNPC fabryka; // Deklaracja zmiennej fabryki NPC
+
+            // Wybór fabryki na podstawie wylosowanego typu postaci
+            switch(typPostaci) {
+                case 0:
+                fabryka = new FabrykaWojownika(); // Ustawienie fabryki na Wojownika
+                break;
+                case 1:
+                fabryka = new FabrykaMaga(); // Ustawienie fabryki na Maga
+                break;
+                case 2:
+                fabryka = new FabrykaZłodzieja(); // Ustawienie fabryki na Złodzieja
+                break;
+                default:
+                throw new Exception("Nieznany typ postaci"); // Obsługa nieznanego typu postaci
+            }
+
+            INPC npc = fabryka.StwórzNPC(); // Tworzenie NPC za pomocą wybranej fabryki
+            npc.PrzedstawSię(); // Wywołanie metody przedstawiającej NPC
+        }
+    }
+}
